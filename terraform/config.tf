af3b31034cf792e069d06b49bad724d106bf6429
Sam Pepper <sezguin@outlook.com>
2024-07-29 17:58:54 +0100

    :recycle: Updated required version operator and the AWS provider version from Hashicorp.

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,12 +1,13 @@
 terraform {
-  required_version = "= 1.9.3"
+  required_version = ">= 1.9.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.60.0"
     }
   }
 }
+
 
 provider "aws" {
   region = var.region
