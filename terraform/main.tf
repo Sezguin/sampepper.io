dbb3f1b2603634efb8bb49b213cd0807d782b045
Sam Pepper <sezguin@outlook.com>
2024-07-29 17:52:28 +0100

    :sparkles: Added bucket_access_block to configure access to the  static site bucket.

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,15 @@ resource "aws_s3_bucket" "static_site_bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket_access_block" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "static_site_bucket_policy" {
   depends_on = [aws_s3_bucket_public_access_block.bucket_access_block]
   bucket     = aws_s3_bucket.static_site_bucket.id
