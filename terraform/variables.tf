7b5fbe935af013b7a900204368d7d4a79b481cb6
Sam Pepper <sezguin@outlook.com>
2024-07-29 18:41:24 +0100

    :negative_squared_cross_mark: Removed unnecessary variables.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,21 +9,3 @@ variable "region" {
   type        = string
   default     = "eu-west-2"
 }
-
-variable "state_bucket_name" {
-  description = "The name of the S3 bucket used for storing Terraform state."
-  type        = string
-  default     = "terraform-state-bucket-online-resume"
-}
-
-variable "state_bucket_key" {
-  description = "The key of the object in the S3 bucket used for storing Terraform state."
-  type        = string
-  default     = "users/sam-pepper/terraform.tfstate"
-}
-
-variable "dynamodb_table" {
-  description = "The name of the DynamoDB table used for state locking."
-  type        = string
-  default     = "terraform-state-lock-table-online-resume-sam-pepper"
-}
