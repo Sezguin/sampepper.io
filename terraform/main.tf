86fe052a8f90ad36a716aadf2d50982de38f7761
Sam Pepper <sezguin@outlook.com>
2024-07-29 19:11:59 +0100

    :recycle: Allow Public Access

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,8 @@ resource "aws_s3_bucket" "static_site_bucket" {
 resource "aws_s3_bucket_public_access_block" "bucket_access_block" {
   bucket = aws_s3_bucket.static_site_bucket.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls   = false
+  block_public_policy = false
   ignore_public_acls  = true
   restrict_public_buckets = true
 }
